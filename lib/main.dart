51a9191c90cae8e48694d4ceb5ed0c0ca4d146ad
Xaidel <abechuelak@gmail.com>
2024-02-12 03:51:05 +0800

    remove const in bodycontainer invocation

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        appBar: TitleBar(),
+        appBar: const TitleBar(),
         body: BodyContainer(),
       ),
     );
